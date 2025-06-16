73ffe275507b9cb99c6be1d9803bcd6dfe9ab626
Cristina <83121356+cristina1810@users.noreply.github.com>
2025-06-16 08:55:32 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //VALORES EN LA CARTA
+  let num = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"];
+  let palo = ["♦","♥","♠","♣"];
+
+function rdm (){
+let numHTML= num[Math.floor(Math.random()*num.length)];
+let paloHTML=palo[Math.floor(Math.random()*palo.length)];
+
+if (paloHTML == "♦" || paloHTML == "♥") {
+  document.getElementById("num").style.color = "red";
+  document.getElementById("paloA").style.color = "red";
+  document.getElementById("paloB").style.color = "red";
+
+}
+  document.getElementById("num").textContent = numHTML;
+  document.getElementById("paloA").textContent = paloHTML;
+  document.getElementById("paloB").textContent = paloHTML;
+}
+
+  rdm()
+
 };
