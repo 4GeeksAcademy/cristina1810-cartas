fb88fd3c8a85393a06d4ec9b3ff66a20317f64e5
Cristina <83121356+cristina1810@users.noreply.github.com>
2025-06-17 10:37:36 +0000

    desafios completados

--- src/app.js
+++ src/app.js
@@ -8,24 +8,52 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   //VALORES EN LA CARTA
-  let num = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"];
-  let palo = ["♦","♥","♠","♣"];
+  const num = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"];
+  const palo = ["♦","♥","♠","♣"];
 
 function rdm (){
-let numHTML= num[Math.floor(Math.random()*num.length)];
-let paloHTML=palo[Math.floor(Math.random()*palo.length)];
+const numHTML= num[Math.floor(Math.random()*num.length)];
+const paloHTML=palo[Math.floor(Math.random()*palo.length)];
 
 if (paloHTML == "♦" || paloHTML == "♥") {
   document.getElementById("num").style.color = "red";
   document.getElementById("paloA").style.color = "red";
   document.getElementById("paloB").style.color = "red";
-
+} else {
+   document.getElementById("num").style.color = "black";
+  document.getElementById("paloA").style.color = "black";
+  document.getElementById("paloB").style.color = "black";
 }
   document.getElementById("num").textContent = numHTML;
   document.getElementById("paloA").textContent = paloHTML;
   document.getElementById("paloB").textContent = paloHTML;
 }
-
   rdm()
+
+document.getElementById("btnGenerar").addEventListener("click", rdm);
+
+//Contador 
+let tiempo = 10;
+setInterval(() => {
+if (tiempo <= 10 && tiempo > 0) {
+  tiempo--;
+  document.getElementById("countd").textContent = tiempo;
+} else {
+  rdm();
+  tiempo = 10;
+  document.getElementById("countd").textContent = tiempo;
+}
+}, 1000);
+
+// Cambio de tamaño de la carta
+//Funciona pero no es responsive
+document.getElementById("btnResize").addEventListener("click", () => {
+const wdth = parseInt(document.getElementById("inputWidth").value);
+const height= parseInt(document.getElementById("inputHeight").value);
+
+document.querySelector(".card").style.width = wdth+"vw";
+document.querySelector(".card").style.height = height+"vw";
+
+});
 
 };
